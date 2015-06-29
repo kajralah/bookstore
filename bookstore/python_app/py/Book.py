b429219f2ed865675e8cdb57bbce1ee67ca4b73b
kajralah <kajralah@gmail.com>
2015-06-29 22:42:28 +0300

    adding getters in Book.py

--- bookstore/python_app/py/Book.py
+++ bookstore/python_app/py/Book.py
@@ -1,16 +1,16 @@
-from bookstore.python_app.py.Exceptions import *
+#from bookstore.python_app.py.Exceptions import *
 
 class Book:
 
-    #add book-img blob ? 
-    def __init__(self,book_title,book_price,book_author,book_pages,book_publisher,book_description,book_category):
-        this.book_title = book_title
-        this.book_price = book_price
-        this.book_author = book_author
-        this.book_pages = book_pages
-        this.book_publisher = book_publisher
-        this.book_description = book_description
-        this.book_category = book_category
+    def __init__(self,book_title,book_price,book_author,book_pages,book_publisher,book_description,book_category,book_image):
+        self.__book_title = book_title
+        self.__book_price = book_price
+        self.__book_author = book_author
+        self.__book_pages = book_pages
+        self.__book_publisher = book_publisher
+        self.__book_description = book_description
+        self.__book_category = book_category
+        self.__book_image = book_image
 
     @staticmethod
     def validating_book(self,book_title,book_price,book_author,book_pages,book_publisher,book_description,book_category):
@@ -18,3 +18,27 @@ class Book:
             return True
         else:
             raise InvalidBookException()
+
+    def book_title(self):
+        return self.__book_title
+
+    def book_price(self):
+        return self.__book_price
+
+    def book_author(self):
+        return self.__book_author
+    
+    def book_pages(self):
+        return self.__book_pages
+
+    def book_publisher(self):
+        return self.__book_publisher
+
+    def book_description(self):
+        return self.__book_description
+
+    def book_category(self):
+        return self.__book_category
+
+    def book_image(self):
+        return self.__book_image    
